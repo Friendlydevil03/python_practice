70f9ac9752f1fe208eda205e02d1fce12dc39313
srivenkatesh <srivenkatesh49175@gmail.com>
2025-10-26 20:46:19 +0530

    atm modified

--- oop/atm-abstraction/atm.py
+++ oop/atm-abstraction/atm.py
@@ -1,3 +1,4 @@
+
 from absta import user_show
 
 class ATM(user_show):
@@ -5,33 +6,48 @@ class ATM(user_show):
         self.name = name
         self.pin_number = pin_number
         self.balance = balance
+        self.auth = False
 
     def card(self, name):
-        if self.name is None:
-            print("Please insert the card")
+        if name == self.name:
+            self.auth = True
+            print(f"Welcome {self.name}")
         else:
-            print(self.name)
+            print("Please insert the card")
+            self.auth = False
 
     def pin(self, pin_number):
-        if self.pin_number.isnumeric():
-            print("Please enter the amount")
-        else:
-            print("Please enter a valid pin")
+        if self.auth is True:
+            str_pin = str(pin_number)
+            if str_pin == self.pin_number:
+                self.auth = True
+                print("verified")
+            else:
+                print("Please enter a valid pin")
+                self.auth = False
 
     def deposit(self,deposit_cash):
-        if deposit_cash > 0 :
-            self.balance += deposit_cash
-            print(f"${deposit_cash} deposited")
+        if self.auth is True:
+            if deposit_cash > 0 :
+                self.balance += deposit_cash
+                print(f"${deposit_cash} deposited")
+            else:
+                print("Enter a valid deposit amount")
         else:
-            print("Enter a valid deposit amount")
+            self.auth = False
 
     def withdraw(self, withdraw_cash):
-        if withdraw_cash < self.balance:
-            self.balance -= withdraw_cash
-            print(f"${withdraw_cash} withdraded")
-        elif withdraw_cash > self.balance:
-            print("Insufficient funds")
-
+        if self.auth is True:
+            if withdraw_cash < self.balance:
+                self.balance -= withdraw_cash
+                print(f"${withdraw_cash} withdraded")
+            elif withdraw_cash > self.balance:
+                print("Insufficient funds")
+        else:
+            self.auth = False
 
     def balance_check(self):
-        print(f"Your bank balance is ${self.balance}")
+        if self.auth is True:
+            print(f"Your bank balance is ${self.balance}")
+        else:
+            self.auth = False
--- oop/atm-abstraction/main.py
+++ oop/atm-abstraction/main.py
@@ -1,5 +1,7 @@
 from atm import ATM
-s = ATM("sri",3333)
-s.deposit(10000)
-s.withdraw(5000)
+s = ATM("sri","3333")
+s.card("sri")
+s.pin(3333)
+s.deposit(100000)
+s.withdraw(50000)
 s.balance_check()
